8bd379ea420b1eb46b36f3d3cacaa8b6052271fa
Gabriel Cootauco <57003600+not-gabrielc@users.noreply.github.com>
2022-08-04 01:06:25 +0100

    figuring out a pseudo element fro img

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import ReactBody from './components/content';
+import { fetchApi } from './components/api';
+import Country from './components/country';
+import IpAddress from './components/ip';
 import './css/App.css';
 
 function App() {
+  const [apiCall, setApiCall] = React.useState();
+  const [buttonClick, setButtonClick] = React.useState(false);
+
+  React.useEffect(() => {
+    fetchApi().then(f => {
+      setApiCall(f)
+      console.log(f)
+    }
+    ).catch(error =>
+      console.error(`Could not get api: ${error}`)
+    )
+  }, []);
+
 
   return (
     <div className="App">
@@ -12,7 +27,33 @@ function App() {
         </h2>
       </header>
 
-      <ReactBody />
+      <div className='showInfo'>
+        <button onClick={() => setButtonClick(!buttonClick)} >
+          {
+            !buttonClick ?
+              <b>Show Info</b>
+              :
+              <b>Close Info</b>
+          }
+        </button>
+      </div>
+      {
+        buttonClick ?
+          <div className='widget'>
+            {
+              apiCall &&
+              <div className='childWidget'>
+                <IpAddress ip={apiCall.ip} />
+                <hr />
+                <Country country={apiCall.country} />
+              </div>
+            }
+          </div>
+          :
+          <p>
+            Click <b>Show Info</b> to open widget.
+          </p>
+      }
 
     </div >
   );
--- src/components/country.js
+++ src/components/country.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import '../css/component.css'
+/* https://www.countryflagsapi.com/ used to get the image of the flag */
 
 
 const Country = (props) => {
-    const { country } = props
+    const { country } = props;
 
     return (
         <div className='mainDiv'>
             <h3 className='keyDiv'>Country:</h3>
             <p className='valueDiv'>{country}</p>
+            <img
+                className='image'
+                style={{ width: '10%' }}
+                src={`https://countryflagsapi.com/png/${country}`}
+                // style="--urlLink: url('https://countryflagsapi.com/png/GB')"
+                alt={country}
+            />
         </div>
     );
 };
