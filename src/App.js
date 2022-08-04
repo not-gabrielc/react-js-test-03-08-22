4599939f32ae864a971d3167f07db60b0ad940c5
Gabriel Cootauco <57003600+not-gabrielc@users.noreply.github.com>
2022-08-04 13:32:49 +0100

    post on mock api

--- src/App.js
+++ src/App.js
@@ -10,12 +10,42 @@ function App() {
   React.useEffect(() => {
     fetchApi().then(f => {
       setApiCall(f)
-      console.log(f)
     }
     ).catch(error =>
       console.error(`Could not get api: ${error}`)
     )
   }, []);
+
+  const postInApi = (e) => {
+    e.preventDefault()
+
+    const postEndPoint = 'https://62ebb8e6705264f263dee407.mockapi.io/info'
+
+    const data = {
+      IpAddress: apiCall.ip,
+      Country: apiCall.country,
+      Flag: `https://countryflagsapi.com/svg/${apiCall.country}`
+    }
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    };
+
+    fetch(postEndPoint, requestOptions)
+      .then(response => {
+        console.log("Status code: " + response.status)
+        return response.json()
+      })
+      .then(response =>
+        console.log(response)
+      )
+      .catch(err => {
+        console.error(err)
+      })
+
+  };
 
 
   return (
@@ -33,6 +63,15 @@ function App() {
             <IpAddress ip={apiCall.ip} />
             <hr />
             <Country country={apiCall.country} />
+
+            {/* send to the api */}
+            <div>
+              <button
+                onClick={e => postInApi(e)}
+              >
+                post
+              </button>
+            </div>
           </div>
         </div>
       }
